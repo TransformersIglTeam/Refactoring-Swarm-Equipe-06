f0a6b3397dcdeb890dd6d5c7e9bd10023027871b
Bakhti Nabil Abdelkafi <nn_bakhti@esi.dz>
2026-01-06 04:07:22 +0100

    fix: file operation tools with changeing the sandbox and the path validation

--- src/tools/file_operations/ReadTool.py
+++ src/tools/file_operations/ReadTool.py
@@ -2,8 +2,8 @@ from langchain.tools import BaseTool
 from pathlib import Path
 from typing import Optional
 
-from ..utils.PathValidator import validate_path
-from ..utils import SandboxSetup
+from src.utils.PathValidator import validate_path
+from src.utils import SandboxSetup
 
 class ReadTool(BaseTool):
     name = "read file"
